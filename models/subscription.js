ed86dd75003f1cf7fae703043cd194e9b8fb2bb5
Buchanan, Anthony (CORP) <Anthony.Buchanan@adp.com>
2017-06-23 17:42:19 -0400

    updated subscription model

--- models/subscription.js
+++ models/subscription.js
@@ -3,17 +3,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-let Location = new Schema({
-	address : String,
-	serviceArea : {type: Schema.Types.ObjectId, ref: 'ServiceArea'}
-})
-
 let Subscription = new Schema({
 	user: {type: Schema.Types.ObjectId, ref: 'User'},
-	amount: String,
+	type: String,
 	payment: {type: Schema.Types.ObjectId, ref: 'Payment'},
-	status: String,
-	location: Location,
+	status: { type: String, default: 'ACTIVE'},
+	serviceArea: {type: Schema.Types.ObjectId, ref: 'ServiceArea'},
 	start_date: {type: Date, default: Date.now},
 	end_date: {type: Date}
 })
