7a0288bba101f5deb7c1413ce3fae1d639ceb18e
Buchanan, Anthony (CORP) <Anthony.Buchanan@adp.com>
2017-06-27 13:50:15 -0400

    added we_pay property to the user object

--- models/user.js
+++ models/user.js
@@ -3,23 +3,34 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+let WePay = new Schema({
+	userId: String,
+	fName: String,
+	lName: String,
+	email: String,
+	access_token: String,
+	accountId: String,
+	referenceId: String
+});
+
 let UserType = new Schema({
 	type: String
-})
+});
 
 let Contact = new Schema({
 	fname: String,
 	lname: String,
 	phone: String,
 	email: String,
-})
+});
 
 let User = new Schema({
 	username : {type: String, unique: true},
 	password : String,
 	user_type : UserType,
 	contact : Contact,
+	we_pay : WePay,
 	register_date : {type: Date, default: Date.now}
-})
+});
 
 module.exports = User = mongoose.model('User', User);
