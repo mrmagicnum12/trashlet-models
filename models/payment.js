a24172979e4892a7a68b44fad075af4a33c5fb98
Buchanan, Anthony (CORP) <Anthony.Buchanan@adp.com>
2017-07-08 15:31:03 -0400

    added card info to payment

--- models/payment.js
+++ models/payment.js
@@ -3,11 +3,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+let CardInfo = new Schema({
+	credit_card_name : String,
+	create_time: Date,
+	reference_id: String,
+	expiration_month: Number,
+	expiration_year: Number,
+	last_four: String,
+})
+
 let Payment = new Schema({
 	type : String,
 	paymentId : String,
 	user : {type : Schema.Types.ObjectId, ref : 'User'},
 	status : String
+	cardInfo : [CardInfo]
 })
 
 module.exports = Payment = mongoose.model('Payment', Payment);
